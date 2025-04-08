381a6502afbaf2ea9335f10913a7b0bbbdcf7414
ilmhubuz <ilmhub.uz@gmail.com>
2025-04-08 08:12:03 +0000

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js
+export default {
+  // ... your other config
+  server: {
+    allowedHosts: ["c4kl6y-5173.csb.app", "https://9kjlgg-5173.csb.app"], // allow your dev URL
+  },
+};
